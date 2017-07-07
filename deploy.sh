b441631676a4f591e19db945a85761374559f5e4
shino <shino@shino.space>
2017-07-07 17:20:08 +0800

    修改了deploy.sh中百度主动推送

--- deploy.sh
+++ deploy.sh
@@ -8,9 +8,9 @@ git add .
 commit_time=$(date "+%Y-%m-%d %H:%M:%S")
 git commit -m "${commit_time}"
 git push -f jekyll master:master
+curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://shino.space&token=WuSqU4rdbH2n2FIX"
 cd ../
 rm -rf _site
 git add .
 git commit -m "$1"
 git push github master:master
-curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://shino.space&token=WuSqU4rdbH2n2FIX"
