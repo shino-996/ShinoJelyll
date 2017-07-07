26006eb56ac371ac9325663c631c6cecf608adda
shino <shino@shino.space>
2017-07-07 17:23:57 +0800

    修正了不能生成urls.txt

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ git add .
 commit_time=$(date "+%Y-%m-%d %H:%M:%S")
 git commit -m "${commit_time}"
 git push -f jekyll master:master
-curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://shino.space&token=WuSqU4rdbH2n2FIX"
+curl -H 'Content-Type:text/plain' --data-binary @urls.xml "http://data.zz.baidu.com/urls?site=https://shino.space&token=WuSqU4rdbH2n2FIX"
 cd ../
 rm -rf _site
 git add .
